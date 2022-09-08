d67df8d04fb84a8c6e690690191347d1a5f2d462
Bryant <bryantmendez8@gmail.com>
2022-09-07 22:31:51 -0500

    added variables to scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#startTimer");
+var questions = document.querySelector("#questions");
+var wrapper = document.querySelector("#wrapper");
+var score = 0;
+var questionIndex = 0;
